96a1c9c02b1671b963465a5fac583506670567d9
xcc410922911 <410922911@qq.com>
2017-09-13 19:32:23 +0800

    点击效果

--- app/src/main/java/com/bb/taold/fragment/MyFragment.java
+++ app/src/main/java/com/bb/taold/fragment/MyFragment.java
@@ -4,12 +4,15 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.bb.taold.R;
 import com.bb.taold.activitiy.my.LoanRecordsActivity;
 import com.bb.taold.base.BaseFragment;
 import com.bb.taold.utils.AppManager;
 
+import butterknife.BindView;
 import butterknife.OnClick;
 
 
@@ -23,6 +26,22 @@ import butterknife.OnClick;
 public class MyFragment extends BaseFragment {
 
 
+    @BindView(R.id.tv_layouttitle_title)
+    TextView mTvLayouttitleTitle;
+    @BindView(R.id.lay_my_header)
+    LinearLayout mLayMyHeader;
+    @BindView(R.id.lay_apply_records)
+    LinearLayout mLayApplyRecords;
+    @BindView(R.id.lay_my_messages)
+    LinearLayout mLayMyMessages;
+    @BindView(R.id.lay_help)
+    LinearLayout mLayHelp;
+    @BindView(R.id.lay_feedback)
+    LinearLayout mLayFeedback;
+    @BindView(R.id.lay_about_us)
+    LinearLayout mLayAboutUs;
+    @BindView(R.id.lay_logout)
+    LinearLayout mLayLogout;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -38,7 +57,7 @@ public class MyFragment extends BaseFragment {
 
     @Override
     public void initView() {
-//        mTvLayouttitleTitle.setText("我的");
+        mTvLayouttitleTitle.setText("我的");
     }
 
     @Override
@@ -47,10 +66,10 @@ public class MyFragment extends BaseFragment {
     }
 
     @OnClick({})
-    public void onViewClicked(View view) {
+    public void onClick(View view) {
         switch (view.getId()) {
             case R.id.lay_my_header:
-                AppManager.getInstance().showActivity(LoanRecordsActivity.class,null);
+                AppManager.getInstance().showActivity(LoanRecordsActivity.class, null);
                 break;
         }
     }
