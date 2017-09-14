e9403edb63de634c16f850723662ecd5583675e8
xcc410922911 <410922911@qq.com>
2017-09-14 20:38:39 +0800

    关于我们

--- app/src/main/java/com/bb/taold/activitiy/my/AboutUsActivity.java
+++ app/src/main/java/com/bb/taold/activitiy/my/AboutUsActivity.java
@@ -1,11 +1,37 @@
 package com.bb.taold.activitiy.my;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.bb.taold.R;
 import com.bb.taold.base.BaseActivity;
 
+import butterknife.BindView;
+import butterknife.OnClick;
+
 public class AboutUsActivity extends BaseActivity {
+
+    @BindView(R.id.btn_back)
+    ImageButton mBtnBack;
+    @BindView(R.id.btn_info)
+    ImageButton mBtnInfo;
+    @BindView(R.id.tv_right)
+    TextView mTvRight;
+    @BindView(R.id.iv_right)
+    ImageView mIvRight;
+    @BindView(R.id.tv_title)
+    TextView mTvTitle;
+    @BindView(R.id.title)
+    RelativeLayout mTitle;
+    @BindView(R.id.tv_version_name)
+    TextView mTvVersionName;
+    @BindView(R.id.lay_check_update)
+    LinearLayout mLayCheckUpdate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +45,7 @@ public class AboutUsActivity extends BaseActivity {
 
     @Override
     public void initView() {
-
+        mTvTitle.setText(R.string.about_us);
     }
 
     @Override
@@ -30,5 +56,13 @@ public class AboutUsActivity extends BaseActivity {
     @Override
     public void initdata() {
 
+    }
+    @OnClick({})
+    public void onClick(View view){
+        switch (view.getId()){
+            case R.id.btn_back:
+                finish();
+                break;
+        }
     }
 }
